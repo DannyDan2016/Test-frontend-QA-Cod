97864e3189756301f7521cf30b2a3655608d4ba3
Danny Parrado <parradodanny@outlook.com>
2025-03-19 18:16:49 -0500

    quitar comentarios innecesarios

--- pages/checkout_information_page.py
+++ pages/checkout_information_page.py
@@ -5,7 +5,7 @@ class CheckoutInformationPage:
         self.page = page
 
     async def fill_information(self, first_name, last_name, postal_code):
-        await self.page.locator("#first-name").fill(first_name)  # ✅ Se usa `await`
-        await self.page.locator("#last-name").fill(last_name)  # ✅ Se usa `await`
-        await self.page.locator("#postal-code").fill(postal_code)  # ✅ Se usa `await`
-        await self.page.locator("#continue").click()  # ✅ Se usa `await` en `click()`
+        await self.page.locator("#first-name").fill(first_name) 
+        await self.page.locator("#last-name").fill(last_name) 
+        await self.page.locator("#postal-code").fill(postal_code)  
+        await self.page.locator("#continue").click() 
