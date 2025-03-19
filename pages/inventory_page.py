bbd52004e94839f33fbe291fef76e37fa763587c
Danny Parrado <parradodanny@outlook.com>
2025-03-19 18:18:02 -0500

    Delete comentarios innecesarios

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -7,8 +7,8 @@ class InventoryPage:
     async def add_to_cart(self, product_name: str):
         product_locator = self.page.locator(f".inventory_item:has-text('{product_name}')")
         add_button = product_locator.locator(".btn_inventory")
-        await add_button.click()  # ✅ Se agregó `await`
+        await add_button.click() 
 
     async def get_cart_count(self) -> str:
         count_element = self.page.locator(".shopping_cart_badge")
-        return await count_element.text_content()  # ✅ Se usa `await` con `text_content()`
+        return await count_element.text_content()  
